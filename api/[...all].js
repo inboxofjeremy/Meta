0d70a336e213a48105d09cda64faf484afcf7416
inboxofjeremy <inboxofjeremys@gmail.com>
2025-12-31 12:15:22 -0800

    Update [...all].js

--- api/[...all].js
+++ api/[...all].js
@@ -1,21 +1,10 @@
+// /api/[...all].js
 export default function handler(req, res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Headers", "*");
 
-  if (req.method === "OPTIONS") {
-    res.status(200).end();
-    return;
-  }
-
-  // If path is exactly manifest.json, return JSON
   if (req.url === "/api/manifest.json") {
-    res.status(200).json({
-      id: "test-addon",
-      version: "1.0.0",
-      name: "Test Addon",
-      resources: [],
-      types: []
-    });
+    res.status(200).json({ test: "ok" });
     return;
   }
 
