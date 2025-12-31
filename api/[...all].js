e6ca11369a95194e79e97461ab402d9f69a088e5
inboxofjeremy <inboxofjeremys@gmail.com>
2025-12-31 12:22:40 -0800

    Update [...all].js

--- api/[...all].js
+++ api/[...all].js
@@ -1,12 +1,8 @@
-// /api/[...all].js
 export default function handler(req, res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Headers", "*");
-
   if (req.url === "/api/manifest.json") {
     res.status(200).json({ test: "ok" });
     return;
   }
-
   res.status(404).send("Not Found");
 }
