0c28812597035475b741016f246ec477cc21f77a
inboxofjeremy <inboxofjeremys@gmail.com>
2025-12-31 14:46:05 -0800

    Update stream.js

--- api/stream.js
+++ api/stream.js
@@ -2,7 +2,8 @@ export default async function handler(req, res) {
   res.setHeader("Content-Type", "application/json");
   res.setHeader("Access-Control-Allow-Origin", "*");
 
-  const { id } = req.query;
+  const { searchParams } = new URL(req.url, `http://${req.headers.host}`);
+  const id = searchParams.get("id");
   if (!id) return res.status(400).json({ error: "Missing id" });
 
   // Example dummy stream
