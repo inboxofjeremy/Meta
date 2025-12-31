a8fb657096d4142966b9170da5022af57aa15157
inboxofjeremy <inboxofjeremys@gmail.com>
2025-12-31 13:55:05 -0800

    Update manifest.js

--- api/manifest.js
+++ api/manifest.js
@@ -1,14 +1,12 @@
 export default function handler(req, res) {
   res.setHeader("Content-Type", "application/json");
-  res.setHeader("Access-Control-Allow-Origin", "*"); // CORS for Stremio
-  res.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.setHeader("Access-Control-Allow-Origin", "*"); // CORS
 
   res.json({
     id: "tvmaze-tmdb-search",
     version: "1.0.0",
     name: "TVMaze + TMDb Search",
-    description: "Search addon using TVMaze + TMDb, returning IMDb IDs for scrapers",
+    description: "Search addon using TVMaze + TMDb, returning IMDb IDs",
     resources: ["meta"],
     types: ["movie", "series"],
     idPrefixes: ["tt", "tvmaze"]
