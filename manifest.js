4f4ba610a59e87d58f60c1a46774912c4b7ae942
inboxofjeremy <inboxofjeremys@gmail.com>
2025-12-31 13:36:47 -0800

    Update and rename api/manifest.js to manifest.js

---
+++ manifest.js
@@ -0,0 +1,13 @@
+{
+  "id": "tvmaze-tmdb-search",
+  "version": "1.0.0",
+  "name": "TVMaze + TMDb Search",
+  "description": "Search addon using TVMaze and TMDb, returning IMDb IDs for scrapers",
+  "resources": ["meta"],
+  "types": ["movie", "series"],
+  "idPrefixes": ["tt", "tvmaze"],
+  "catalogs": [],
+  "behaviorHints": {
+    "configurable": false
+  }
+}
